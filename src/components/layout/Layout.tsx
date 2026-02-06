8a28e751da8b0d3a3a6d7317806e9c13ea40abeb
dhavishka adhikari <dhavishkaadhikari@gmail.com>
2026-02-06 16:09:27 +0530

    remove white space between hero section and footer

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="w-full h-full flex flex-col">
       <Navbar />
-      <main className="flex-1 pt-16 md:pt-20 flex flex-col">{children}</main>
+      <main className="flex-1 pt-16 md:pt-20 flex flex-col m-0 p-0">{children}</main>
       <Footer />
     </div>
   );
